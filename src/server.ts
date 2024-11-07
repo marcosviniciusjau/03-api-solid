ef13d5d4b9e9f3dd96550f5606f0a03c917bf800
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-11-07 11:20:01 -0300

    using prisma to create user and gym tables

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
-import {app} from './app';
+import { app } from './app'
+import { env } from './env'
 
-app.listen({
-  host: '0.0.0.0',
-  port: 3333
-}).then(() => {
-  console.log('HTTP server running on http://localhost:3333')
-})
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log(`HTTP server running on http://localhost:${env.PORT}`)
+  })
